c7d19e537939e1e09b7fda584da97b5b6934effe
Jotony Guerra <jotonyguerra@gmail.com>
2017-09-30 12:25:52 -0700

    now case insensitive

--- code.rb
+++ code.rb
@@ -2,7 +2,13 @@ require 'pry'
 def caesar_cipher(string, number)
   #need to make it more linear notation
   alphabet   = Array('a'..'z')
-  encrypter  = Hash[alphabet.zip(alphabet.rotate(number))]
+  lowercase = Hash[alphabet.zip(alphabet.rotate(number))]
+  alphabet = Array('A'..'Z')
+  uppercase = Hash[alphabet.zip(alphabet.rotate(number))]
+  encrypter = lowercase.merge(uppercase)
+
+
+
   new_string = string.chars.map { |c| encrypter.fetch(c, " ") }
   puts new_string.join("")
   return new_string.join("")
